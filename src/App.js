3ac65ca9f9813cf5cde52caf75f820140ef1c8a9
Dani Bagley <bagley.a.danielle@gmail.com>
2023-02-06 18:38:24 -0700

    Inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useQuery, gql } from "@apollo/client";
 
 function App() {
+  const GET_ARTIST = gql`
+    query GetArtist {
+      Beyonce {
+        name
+        image
+        albums {
+          name
+        }
+      }
+    }
+  `;
+
+  const getArtistQ = () => {
+    const { loading, error, data } = useQuery(GET_ARTIST);
+
+    if (loading) return "Loading...";
+    if (error) return `Error! ${error.message}`;
+
+    return data;
+  };
+
+  console.log(getArtistQ());
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>My first Apollo app 🚀</h2>
+      </div>
     </div>
   );
 }
